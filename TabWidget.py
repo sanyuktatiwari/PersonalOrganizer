ec80d22bbfd2989f3a70891cb2d303f95fcdf65c
Sanyukta Tiwari <sanyuktatiwari21@gmail.com>
2021-10-23 23:15:54 +0530

    Added Delete and View Functionality

--- TabWidget.py
+++ TabWidget.py
@@ -3,35 +3,45 @@ from PyQt5.QtGui import *
 import sys
 from PyQt5.Qt import QTabWidget, QWidget, QFormLayout, QLineEdit, QApplication,\
     QPushButton, QTextEdit, QComboBox
+import functools
+import operator
 from functools import partial
 from openpyxl import Workbook
 import glob
 import openpyxl
 from datetime import date
+from datetime import datetime
+import string
+from torch._C import NoneType
+import os
+from PyQt5 import QtCore
+from PyQt5.QtCore import *
     
 class tabFrame(QTabWidget):
     def __init__(self, parent = None):
         super(tabFrame,self).__init__(parent)
         self.createTab = QWidget()
         self.insertTab = QWidget()
-        self.editTab = QWidget()
+        #self.editTab = QWidget()
         self.deleteTab = QWidget()
         self.viewTab = QWidget()
         
         self.addTab(self.createTab, "CREATE")
         self.addTab(self.insertTab, "INSERT")
-        self.addTab(self.editTab, "EDIT")
+        #self.addTab(self.editTab, "EDIT")
         self.addTab(self.deleteTab,"DELETE")
         self.addTab(self.viewTab, "VIEW")
         
         self.createTabUI()
         self.insertTabUI()
         #self.editTabUI()
-        #self.deleteTabUI()
-        #self.viewTabUI()
+        self.deleteTabUI()
+        self.viewTabUI()
         
         self.setWindowTitle("PERSONAL ORGANIZER")
         self.resize(500,300)
+
+        #self.currentChanged.connect(self, QtCore. SIGNAL(currentChanged(int)), this, SLOT(self.tabChangedSlot()))
         
     def createTabUI(self):
         layout = QFormLayout()
@@ -96,6 +106,66 @@ class tabFrame(QTabWidget):
         self.textToEnter.clear()
         wb.save(str(self.dropDownWBList.currentText()))
         wb.close()
+        
+    def viewTabUI(self):
+        layout = QFormLayout()
+        self.dropDownWBList = QComboBox()
+        workbooks = []
+        for file in glob.glob("*.xlsx"):
+            workbooks.append(file)
+        for workbook in workbooks:
+            self.dropDownWBList.addItem(workbook)
+        layout.addRow("Workbooks to view", self.dropDownWBList)
+        
+        self.textToView = QTextEdit()
+        self.viewData()
+        self.dropDownWBList.currentIndexChanged.connect(self.onViewComboBoxSelected)
+        
+        layout.addRow("Worksheet Data",self.textToView)
+        
+        self.viewTab.setLayout(layout)
+        
+    def onViewComboBoxSelected(self):
+       self.viewData()
+        
+    def viewData(self):
+        #View Text Edit Field
+        data = ""
+        wb = openpyxl.load_workbook(str(self.dropDownWBList.currentText()))
+        sheet = wb.active
+        for value in sheet.iter_rows(min_row=1, max_row=100, min_col=1, max_col=3, values_only=True):
+            for entry in value:
+                if type(entry) == datetime:
+                    data = data + entry.strftime("%m/%d/%Y") + "\t"
+                elif type(entry) == str:
+                    data = data + entry
+            data = data + "\n"
+        self.textToView.setPlainText(data)
+        
+    def deleteTabUI(self):
+        layout = QFormLayout()
+        self.dropDownWBList = QComboBox()
+        workbooks = []
+        for file in glob.glob("*.xlsx"):
+            workbooks.append(file)
+        for workbook in workbooks:
+            self.dropDownWBList.addItem(workbook)
+        layout.addRow("Workbooks available", self.dropDownWBList)
+        
+        #Push button
+        pushButton = QPushButton()
+        pushButton.setText("DELETE")
+        layout.addRow("", pushButton)
+        pushButton.clicked.connect(self.onDeleteTextclick)
+        layout.addRow("Delete Workbook", pushButton)
+        
+        self.deleteTab.setLayout(layout)
+        
+    def onDeleteTextclick(self):
+        os.remove(str(self.dropDownWBList.currentText()))
+        
+    def tabChangedSlot(self):
+        self.availableWorkbooks()
 
 def main():
     app = QApplication(sys.argv)
